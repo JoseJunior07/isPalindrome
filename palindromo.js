27d35bad55a5ba53485eb3a1da562698555f3048
Júnior Cruz <josecdcjunior@gmail.com>
2023-08-09 15:43:50 -0300

    Criação da função isPalindrome

---
+++ palindromo.js
@@ -0,0 +1,14 @@
+function isPalindrome () {
+    let palavra = "reve";
+    let separaPalavra = palavra.split("");
+    let palavraInvertida = separaPalavra.reverse();
+    palavraInvertida = palavraInvertida.join("");
+    
+    if (palavra == palavraInvertida) {
+        console.log(`A palavra ${palavra} é um palíndromo!`);
+    } else {
+        console.log(`A palavra ${palavra} não é um palíndromo!`);
+    }
+}
+
+isPalindrome();
